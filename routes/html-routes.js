7aaa658586053295fcff6471f8c05ce459aba80a
James Antley <jimmy.antley@gmail.com>
2021-08-05 23:29:17 -0400

    added stats html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,15 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Route for exercise entry page
-router.get("/exercise", (req, res) => res.sendFile(path.join(__dirname, "../public/exercise.html")));
+//Route for exercise entry page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+//Route for stats
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 
 module.exports = router;
