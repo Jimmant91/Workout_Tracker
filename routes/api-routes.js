dd8870b3a76efd0dfb04251cd215ff4cf3a6a85b
James Antley <jimmy.antley@gmail.com>
2021-08-09 23:58:03 -0400

    official readme

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const db = require("../models");
 
-//Get all workouts
-router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-    .then(dbWorkout => {
-        res.json(dbWorkout)
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
-});
+// //Get all workouts
+// router.get("/api/workouts", (req, res) => {
+//     db.Workout.find({})
+//     .then(dbWorkout => {
+//         res.json(dbWorkout)
+//     })
+//     .catch(err => {
+//         res.status(400).json(err);
+//     });
+// });
 
 
 //Enter new workout
